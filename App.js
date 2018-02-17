5686f6ae87604ddcb68e97bf3b22a3d85be891d4
Karan Wadhwa <wadhwa.karan96@gmail.com>
2018-02-17 13:09:11 +0530

    load fonts for expo before rendering actual page

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { StyleSheet, Platform, StatusBar, Text, View } from 'react-native';
-import { Container, Header, Title, Content, Body, Right, Left, Icon, Button } from 'native-base';
+import { Spinner, Container, Header, Title, Content, Body, Right, Left, Icon, Button } from 'native-base';
 import { Font } from "expo";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ loading: false });
+  }
 
   render() {
+    if (this.state.loading) {
+      return (
+        <View>
+          <Spinner color='#FF5700'/>
+        </View>
+      );
+    }
     return (
       <View style={styles.container}>
         <Header style={styles.header}>{/*
